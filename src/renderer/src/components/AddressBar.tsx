195e1e43720cf665578872e8d98e5d653059f422
shritt <shriram.thirukkumaran@gmail.com>
2025-03-01 12:22:59 +0530

    feat: enhance AddressBar component with new icon and display current page information

--- src/renderer/src/components/AddressBar.tsx
+++ src/renderer/src/components/AddressBar.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { House, Grip, ArrowRight } from 'lucide-react'
+import { House, Grip, ArrowRight, Squircle } from 'lucide-react'
 
 const AddressBar = ({
   url,
@@ -14,18 +14,17 @@ const AddressBar = ({
   return (
     <>
       <div
-        className="p-2 w-1 h-1 rounded-full bg-black absolute no-drag z-10 top-4 left-4 hover:cursor-pointer"
+        className="absolute w-1 h-1 bg-black rounded-full border border-gray-700 p-2 no-drag z-10 top-4 left-4 hover:cursor-pointer"
         onClick={(e) => {
           e.preventDefault()
-          console.log(e)
           setIsExpanded(!isExpanded)
         }}
       ></div>
       {!isExpanded && (
-        <div className='absolute w-1/3 top-10 left-4   p-2  border-bottom-dark  rounded-md  bg-black text-white'>
+        <div className='absolute w-1/3 top-10 left-4  border border-gray-700 p-2  border-bottom-dark  rounded-md  bg-black text-white'>
           <div className="flex items-center pb-2 pt-1">
             <div className="drag hover:cursor-grab active:cursor-grabbing mr-2">
-              <Grip size={16} />
+              <Grip size={12} />
             </div>
             <div
               className="hover:cursor-pointer"
@@ -34,7 +33,7 @@ const AddressBar = ({
                 setUrl('')
               }}
             >
-              <House size={16} />
+              <House size={12} />
             </div>
           </div>
           <nav className="navbar">
@@ -48,7 +47,7 @@ const AddressBar = ({
               <input
                 type="text"
                 placeholder="Enter a website name"
-                className="placeholder:text-gray-500 form-control border border-gray-700 rounded-sm grow mr-2 no-drag"
+                className="placeholder:text-gray-500 text-sm form-control border border-gray-700 rounded-sm grow mr-2 no-drag"
                 value={value}
                 onChange={(e) => setValue(e.target.value)}
               />
@@ -56,10 +55,19 @@ const AddressBar = ({
                 className="btn p-1 px-2 border border-gray-700 rounded-sm hover:cursor-pointer no-drag"
                 type="submit"
               >
-                <ArrowRight size={16} />
+                <ArrowRight size={12} />
               </button>
             </form>
           </nav>
+          <div>
+            <div className="mt-2">
+              {console.log(document.querySelector('webview'))}
+              <div className="text-xs text-gray-400">Current Page Title:</div>
+              <div className="text-sm">{document.title}</div>
+              <div className="text-xs text-gray-400 mt-1">Current Page URL:</div>
+              <div className="text-sm">{url}</div>
+            </div>
+          </div>
         </div>
       )}
     </>
